134396d4795ef53e10087e30dce50aeeb9b3e82e
Wyatt Preul <wpreul@gmail.com>
2014-06-28 15:26:03 +0100

    Adding examples

---
+++ file.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+
+
+var server = new Hapi.Server(8000, { files: { relativeTo: __dirname } });
+
+var filePath = function (request) {
+
+    return './' + request.params.file;
+};
+
+server.route([
+    { method: 'GET', path: '/{file}', handler: { file: filePath } }
+]);
+
+server.start();
--- route.js
+++ route.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', 8000);
 
 server.route({
- 	method: 'GET',
+    method: 'GET',
     path: '/hello/{name}',
     handler: function (request, reply) {
       reply('Hello ' + request.params.name);
