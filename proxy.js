f54edeb84bca0fa2c7bba7369a602f2eb4541a16
Wyatt Preul <wpreul@gmail.com>
2014-06-28 13:38:49 +0100

    Adding proxy example

---
+++ proxy.js
@@ -0,0 +1,13 @@
+var Hapi = require('hapi');
+
+
+var server = new Hapi.Server(8000);
+
+var mapper = function (request, callback) {
+
+    callback(null, 'http://www.google.com/search?q=' + request.params.term);
+};
+
+server.route({ method: '*', path: '/', handler: { proxy: { host: 'google.com', port: 80, redirects: 5 } } });
+server.route({ method: 'GET', path: '/{term}', handler: { proxy: { mapUri: mapper } } });
+server.start();
